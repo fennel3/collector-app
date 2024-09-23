de57939115cfb4077d3d275189c575ad7d3030ea
Finn <finley.champion@gmail.com>
2024-09-23 12:02:03 +0100

    added if statements so if result is true it returns

--- db_structures.php
+++ db_structures.php
@@ -11,22 +11,24 @@ $dog_query = $db->prepare("SELECT * FROM `dogs`");
 
 $result = $dog_query->execute();
 
+if ($result){
 $data = $dog_query -> fetchAll();
 echo '<pre>';
-var_dump($data);
+var_dump($data);}
 
 $cat_query = $db->prepare("SELECT * FROM `cats`");
 
 $result = $cat_query->execute();
-
+if ($result){
 $data = $cat_query -> fetchAll();
 echo '<pre>';
-var_dump($data);
+var_dump($data);}
 
 $owners_query = $db->prepare("SELECT * FROM `owners`");
 
 $result = $owners_query->execute();
 
+if ($result){
 $data = $owners_query -> fetchAll();
 echo '<pre>';
-var_dump($data);
+var_dump($data);}
