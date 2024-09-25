ee03f84aec5a84c64b77bb47c5769c5e5b44a747
Finn <finley.champion@gmail.com>
2024-09-25 13:57:18 +0100

    added gitignore

--- src/db_structures.php
+++ src/db_structures.php
@@ -15,25 +15,25 @@ function data(): PDO
 //function that returns only the dog array from database
 function getDogData(PDO $db)
 {
-    $dog_query = $db->prepare("SELECT id, owner_id, name, breed FROM pet_names WHERE species_id = 2;");
+    $dog_query = $db->prepare("SELECT `id`, `owner_id`, `name`, `breed` FROM `pet_names` WHERE `species_id` = 2;");
     $dog_result = $dog_query->execute();
 
     if ($dog_result) {
         $dog_data = $dog_query->fetchAll();
         return $dog_data;
-    }
+    } else return 'could not return dog data';
 }
 
 //function that returns only the cat array from database
 function getCatData(PDO $db)
 {
-    $cat_query =$db->prepare("SELECT id, owner_id, name, breed FROM pet_names WHERE species_id = 1;");
+    $cat_query =$db->prepare("SELECT `id`, `owner_id`, `name`, `breed` FROM `pet_names` WHERE `species_id` = 1;");
 
     $cat_result = $cat_query->execute();
     if ($cat_result){
         $cat_data = $cat_query -> fetchAll();
         return $cat_data;
-    }
+    } else return 'could not return cat data';
 }
 //function that returns only the owner array from database
 function getOwnerData(PDO $db)
@@ -45,7 +45,7 @@ function getOwnerData(PDO $db)
     if ($owner_result){
         $owner_data = $owners_query -> fetchAll();
         return $owner_data;
-    }
+    } else return 'could not return owner data';
 }
 
 
